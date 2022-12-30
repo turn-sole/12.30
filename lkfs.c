e4bace8ea8da1f7dc1a791c727160ce12f682501
turn-sole <113235914+turn-sole@users.noreply.github.com>
2022-12-30 20:45:48 +0800

    Add files via upload

---
+++ lkfs.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+//int x = 5, y = 7;
+//void swap()
+//{
+//	int z;
+//	z = x;
+//	x = y;
+//	y = z;
+//}
+//int main()
+//{
+//	int x = 3, y = 8;
+//	swap();
+//	printf("%d,%d\n", x, y);
+//	return 0;
+//}
+//int main()
+//{
+//	double x[5] = { 2.0,4.0,6.0,8.0,10.0 };
+//	char c2[] = { '\x10','\xa','\8' };
+//	char c1[] = { '1','2','3','4','5' };
+//	int y[5 + 3] = { 0,1,3,5,7,9 };
+//}
